1d7492a3c41199ef34b23b4e92b91609839d4a89
p_penkovskiy <p.penkovskiy@gmail.com>
2022-01-15 15:21:29 +0300

    commit10

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='name')
     content = models.TextField(blank=True)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
     time_create = models.DateTimeField(auto_now_add=True)
@@ -16,6 +16,11 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = 'Famous women'
+        verbose_name_plural = 'Famous women'
+        ordering = ['time_create', 'title']
+
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
 
@@ -24,3 +29,8 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Category'
+        ordering = ['id']
