ec09254ab88e98007a4969132098b9ef2be0d02a
p_penkovskiy <p.penkovskiy@gmail.com>
2022-01-15 13:04:58 +0300

    commit8

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -3,9 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('about/', about, name='about')
-    # path('categories/<int:category_id>/', categories),
-    # re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
 ]
 
 
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -2,22 +2,31 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import redirect, render
 from .models import *
 
-menu = ['About page', 'Add a blog', 'Feedback', 'Sign In']
+menu = [{'title': "About page", "url_name": "about"},
+        {'title': "Add a blog", "url_name": "add_page"},
+        {'title': "Feedback", "url_name": "contact"},
+        {'title': "Login", "url_name": "login"},
+        ]
 
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Home page'})
+    context = {'posts': posts, 'menu': menu, 'title': 'Home page'}
+    return render(request, 'women/index.html', context=context)
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'About page'})
 
-def categories(request, category_id):
-    return HttpResponse(f'<h1>Categories page</h1><p>{category_id}</p>')
-
-def archive(request, year):
-    if int(year) > 2021 :
-        return redirect('home', permanent=True)
-    return HttpResponse(f'<h1>Archive by year</h1><p>{year}</p>')
-
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Page not found.</h1><h1>Please, try again.</h1>')
+
+def addpage(request):
+    return HttpResponseNotFound('Add page')
+
+def contact(request):
+    return HttpResponseNotFound('Contact us')
+
+def login(request):
+    return HttpResponseNotFound('Login')
+
+def show_post(request, post_id):
+    return HttpResponse(f"Post with id = {post_id}")
