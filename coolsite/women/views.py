8791bc4ba8f6694ccad0febf2c9895e905b83080
p_penkovskiy <p.penkovskiy@gmail.com>
2022-01-20 21:51:25 +0300

    commit15

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,39 +1,51 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import redirect, render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+
 from .models import *
 from .forms import *
 
-menu = [{'title': "About page", "url_name": "about"},
+menu = [{'title': "About", "url_name": "about"},
         {'title': "Add article", "url_name": "add_page"},
         {'title': "Contacts", "url_name": "contact"},
         {'title': "Login", "url_name": "login"},
         ]
 
-def index(request):
-    posts = Women.objects.all()
-    context = {
-               'posts': posts,
-               'menu': menu,
-               'title': 'Home page',
-               'cat_selected': 0
-    }
-    return render(request, 'women/index.html', context=context)
+
+class WomenHome(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Main page'
+        context['cat_selected'] = 0
+        return context
+
+    def get_queryset(self):
+        return Women.objects.filter(is_published=True)
+
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'About page'})
 
+
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Page not found.</h1><h1>Please, try again.</h1>')
 
-def addpage(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = AddPostForm()
-    return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Add article'})
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'women/addpage.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Add article'
+        context['menu'] = menu
+        return context
 
 def contact(request):
     return HttpResponseNotFound('Contact us')
@@ -41,24 +53,30 @@ def contact(request):
 def login(request):
     return HttpResponseNotFound('Login')
 
-def show_post(request, post_slug):
-    post = get_object_or_404(Women, slug=post_slug)
-    context = {
-        'post': post,
-        'menu': menu,
-        'title': post.title,
-        'cat_selected': post.cat_id,
-    }
-    return render(request, 'women/post.html', context=context)
+class ShowPost(DetailView):
+    model = Women
+    template_name = 'women/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
 
-def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id)
-    if len(posts) == 0:
-        raise Http404()
-    context = {
-               'posts': posts,
-               'menu': menu,
-               'title': 'Categories',
-               'cat_selected': cat_id
-    }
-    return render(request, 'women/index.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['post']
+        context['menu'] = menu
+        return context
+
+class WomenCategory(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Category' + str(context['posts'][0].cat)
+        context['menu'] = menu
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
