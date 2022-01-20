ea08419c3768f703fbb2b6b63b03eca94916a3cb
p_penkovskiy <p.penkovskiy@gmail.com>
2022-01-20 15:13:34 +0300

    commit13

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -3,18 +3,19 @@ from django.urls import reverse
 
 class Women(models.Model):
     title = models.CharField(max_length=255, verbose_name='name')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     content = models.TextField(blank=True)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post', kwargs={'post_id': self.pk})
+        return reverse('post', kwargs={'post_slug': self.slug})
 
     class Meta:
         verbose_name = 'Famous women'
@@ -23,6 +24,7 @@ class Women(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
 
     def __str__(self):
         return self.name
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import *
+from .forms import *
 
 menu = [{'title': "About page", "url_name": "about"},
         {'title': "Add a blog", "url_name": "add_page"},
@@ -27,7 +28,17 @@ def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Page not found.</h1><h1>Please, try again.</h1>')
 
 def addpage(request):
-    return HttpResponseNotFound('Add page')
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Post add error')
+    else:
+        form = AddPostForm()
+    return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Add article'})
 
 def contact(request):
     return HttpResponseNotFound('Contact us')
@@ -35,20 +46,24 @@ def contact(request):
 def login(request):
     return HttpResponseNotFound('Login')
 
-def show_post(request, post_id):
-    return HttpResponse(f"Post with id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+    return render(request, 'women/post.html', context=context)
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-
     if len(posts) == 0:
         raise Http404()
-
     context = {
                'posts': posts,
                'menu': menu,
                'title': 'Categories',
-               'cat_selected': 0
+               'cat_selected': cat_id
     }
-
     return render(request, 'women/index.html', context=context)
